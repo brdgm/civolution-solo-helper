7ce17aa5791da8e38aa6b48faf212ab8426bee71
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-10-20 08:02:06 +0200

    assets

---
+++ src/services/enum/Territory.ts
@@ -0,0 +1,13 @@
+/**
+ * Territory
+ */
+enum Territory {
+  FOREST = 'forest',
+  GRASSLAND = 'grassland',
+  HILLS = 'hills',
+  SWAMP = 'swamp',
+  MOUNTAINS = 'mountains',
+  DESERT = 'desert',
+  WATER = 'water'
+}
+export default Territory
