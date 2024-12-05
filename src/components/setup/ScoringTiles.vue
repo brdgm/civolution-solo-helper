0f15f6ffc5a5320c5a18d1362d1657eff7394a4e
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-12-05 18:27:11 +0100

    Allow manual selection of scoring tiles (#8)

--- src/components/setup/ScoringTiles.vue
+++ src/components/setup/ScoringTiles.vue
@@ -13,38 +13,66 @@
           class="tile" :class="{[`item${index}`]:true}"/>
     </div>
 
-    <button class="btn btn-sm btn-outline-secondary" @click="randomizeScoringTiles">{{t('setup.scoringTiles.randomize')}}</button>
+    <button class="btn btn-sm btn-outline-secondary me-2" data-bs-toggle="modal" data-bs-target="#scoringTilesModal">{{t('setup.scoringTiles.select')}}</button>
+    <button class="btn btn-sm btn-outline-secondary me-2" @click="randomizeScoringTiles">{{t('setup.scoringTiles.randomize')}}</button>
   </div>
+
+  <ModalDialog id="scoringTilesModal" :title="t('setup.scoringTiles.title')" :size-lg="true">
+    <template #body>
+      {{t('setup.scoringTiles.available')}}<br/>
+      <AppIcon v-for="tile of scoringTilesAllWithoutSelection" :key="tile" type="scoring-tile" :name="tile"
+          class="tile select" @click="selectScoringTile(tile)"/>
+      <hr/>
+      {{t('setup.scoringTiles.selected')}}<br/>
+      <template v-for="(tile,index) of scoringTilesSelection" :key="tile">
+        <span v-if="index == 3" class="separator">|</span>
+        <AppIcon  type="scoring-tile" :name="tile" class="tile select" @click="deselectScoringTile(tile)"/>
+      </template>
+      <p v-if="scoringTilesSelection.length == 0" class="fst-italic">
+        {{t('setup.scoringTiles.none')}}
+      </p>
+    </template>
+    <template #footer>
+      <button class="btn btn-outline-secondary" @click="scoringTilesSelection=[]">{{t('action.reset')}}</button>
+      <button class="btn btn-success" data-bs-dismiss="modal" :disabled="scoringTilesSelection.length != 7" @click="setScoringRoundTileSelection">{{t('setup.scoringTiles.select')}}</button>
+      <button class="btn btn-secondary" data-bs-dismiss="modal">{{t('action.cancel')}}</button>
+    </template>
+  </ModalDialog>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useStateStore } from '@/store/state'
 import ScoringCategory from '@/services/enum/ScoringCategory'
 import getAllEnumValues from '@brdgm/brdgm-commons/src/util/enum/getAllEnumValues'
 import { shuffle } from 'lodash'
 import AppIcon from '../structure/AppIcon.vue'
+import ModalDialog from '@brdgm/brdgm-commons/src/components/structure/ModalDialog.vue'
 
 export default defineComponent({
   name: 'ScoringTiles',
   components: {
-    AppIcon
+    AppIcon,
+    ModalDialog
   },
   setup() {
     const { t } = useI18n()
     const state = useStateStore()
 
+    const allScoringTiles = getAllEnumValues(ScoringCategory)
+    const scoringTilesSelection = ref([] as ScoringCategory[])
+
     const randomizeScoringTiles = function() {
-      const allScoringTiles = shuffle(getAllEnumValues(ScoringCategory))
-      state.setup.eraScoringTiles = allScoringTiles.slice(0, 4)
-      state.setup.finalScoringTiles = allScoringTiles.slice(4, 7)
+      const allScoringTilesShuffled = shuffle(allScoringTiles)
+      state.setup.finalScoringTiles = allScoringTilesShuffled.slice(0, 3)
+      state.setup.eraScoringTiles = allScoringTilesShuffled.slice(3, 7)
     }
     if (state.setup.eraScoringTiles.length === 0 || state.setup.finalScoringTiles.length === 0) {
       randomizeScoringTiles()
     }
 
-    return { t, state, randomizeScoringTiles }
+    return { t, state, allScoringTiles, scoringTilesSelection, randomizeScoringTiles }
   },
   computed: {
     eraScoringTiles() {
@@ -52,6 +80,23 @@ export default defineComponent({
     },
     finalScoringTiles() {
       return this.state.setup.finalScoringTiles
+    },
+    scoringTilesAllWithoutSelection() : ScoringCategory[] {
+      return this.allScoringTiles.filter(tile => !this.scoringTilesSelection.includes(tile))
+    }
+  },
+  methods: {
+    selectScoringTile(tile: ScoringCategory) : void {
+      if (this.scoringTilesSelection.length < 7) {
+        this.scoringTilesSelection.push(tile)
+      }
+    },
+    deselectScoringTile(tile: ScoringCategory) : void {
+      this.scoringTilesSelection = this.scoringTilesSelection.filter(t => t != tile)
+    },
+    setScoringRoundTileSelection() : void {
+      this.state.setup.finalScoringTiles = this.scoringTilesSelection.slice(0, 3)
+      this.state.setup.eraScoringTiles = this.scoringTilesSelection.slice(3, 7)
     }
   }
 })
@@ -124,5 +169,21 @@ export default defineComponent({
 .tile {
   height: 47px;
   box-shadow: 0 0 5px 5px rgb(255 0 0 / 100%);
+}
+#scoringTilesModal {
+  .tile {
+    box-shadow: unset;
+    margin-right: 0.5rem;
+    margin-bottom: 0.5rem;
+    filter: drop-shadow(2px 2px 2px #888);
+    &.select {
+      cursor: pointer;
+    }
+  }
+  .separator {
+    font-size: 30px;
+    margin-left: 0.5rem;
+    margin-right: 1rem;
+  }
 }
 </style>
